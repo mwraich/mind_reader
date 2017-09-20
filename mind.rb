a4ae04661fbf53da76331b3b5dec6e0eb67b7ab4
Bryan MacDiarmid <bryanm@nulogy.com>
2017-09-20 15:45:07 -0400

    Mind responds to guesses with higher or lower

--- mind.rb
+++ mind.rb
@@ -1,8 +1,14 @@
 class Mind
+  LOWER = "LOWER"
+  HIGHER = "HIGHER"
+
   def initialize(choice)
+    @choice = choice
   end
 
   def respond_to_guess(guess)
-    true
+    return true if guess == @choice
+    return LOWER if guess > @choice
+    return HIGHER if guess < @choice
   end
 end
--- spec/mind_spec.rb
+++ spec/mind_spec.rb
@@ -9,6 +9,20 @@ describe Mind do
     expect(result).to eq(true)
   end
 
-  xit "returns higher when offered a guess lower than it's choice" do
+  it "returns lower when offered a guess higher than it's choice" do
+    mind = Mind.new(1)
+
+    result = mind.respond_to_guess(2)
+
+    expect(result).to eq("LOWER")
   end
+
+  it "returns higher when offered a guess lower than it's choice" do
+    mind = Mind.new(2)
+
+    result = mind.respond_to_guess(1)
+
+    expect(result).to eq("HIGHER")
+  end
+
 end
