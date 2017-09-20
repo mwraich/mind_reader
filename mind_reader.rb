6d5ee26fa80f3335156a631c645847f26189044d
‘Mandeep <mandeepw@nulogy.com>
2017-09-20 14:50:12 -0400

    pass mind_reader a range max, update spec

--- mind_reader.rb
+++ mind_reader.rb
@@ -1,5 +1,11 @@
 class MindReader
-  def self.guess(number)
+  attr_reader :range_max
+
+  def initialize(range_max)
+    @range_max = range_max
+  end
+
+  def guess(number)
     number
   end
 end
--- spec/mind_reader_spec.rb
+++ spec/mind_reader_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../mind_reader'
 describe MindReader do
   it 'should accept a numerical input' do
-      expect(MindReader.guess(1)).to eq(1)
+    mind_reader = MindReader.new(1)
+    expect(mind_reader.guess(1)).to eq(1)
   end
 end
