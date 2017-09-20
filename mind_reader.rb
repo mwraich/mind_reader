a7b847ed5bfd0cf7077af2e40768d13f09a5d630
‘Mandeep <mandeepw@nulogy.com>
2017-09-20 15:15:12 -0400

    makes range method private, clean up spec

--- mind_reader.rb
+++ mind_reader.rb
@@ -5,12 +5,14 @@ class MindReader
     @range_max = range_max
   end
 
-  def range
-    1..range_max
-  end
-
   def guess(number)
     raise 'Not Within Range' unless range.include?(number)
     number
+  end
+
+  private
+
+  def range
+    1..range_max
   end
 end
